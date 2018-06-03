2746696b28dd36a076819043a3a79d7b642cd315
YoniProbeh <34262714+YoniProbeh@users.noreply.github.com>
2018-06-03 21:03:35 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Search Input
+const searchUser = document.getElementById('searchUser').addEventListener('keyup', (event) => {
+  // Get Text Input
+  const userText = event.target.value;
+  // Validating That The Text Input Is Not An Empty Value
+  if (userText !== '') {
+    // Fetch Data From Github API Based On Input Criteria
+  }
+});
